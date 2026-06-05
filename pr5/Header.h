5a6327c296516ecf6d1ca8eaa9219c30becd507d
Mr_Bryanda_St <58851457+bryndaSait@users.noreply.github.com>
2026-06-05 12:45:28 +0300

    Update Header.h

--- pr5/Header.h
+++ pr5/Header.h
@@ -18,12 +18,12 @@ protected:
 public:
 	CVector(int s = 0, double* arr = nullptr);
 	CVector(const CVector& other);
-	CVector(CVector&& other);
+	//
 	~CVector();
 
 	void copy(int s, double* arr);
 	void copy(const CVector& other);
-
+	//
 	void out() const;
 	virtual void vout() const;
 	void clear();
@@ -34,7 +34,10 @@ public:
 	void diff(const CVector& other);
 
 	CVector& operator=(const CVector& other);
-	CVector& operator=(CVector&& other);
+	CVector(CVector&& other) noexcept;
+	CVector& operator=(CVector&& other) noexcept;
+	int move(CVector& other);//
+
 	double& operator[](int index);
 	const double operator[](int index) const;
 
@@ -49,17 +52,5 @@ CVector operator+(const CVector& v1, const CVector& v2);
 CVector operator-(const CVector& v1, const CVector& v2);
 double operator*(const CVector& v1, const CVector& v2);
 
-class CVector2 :public CVector {
-public:
-	CVector2(const CVector2& other);
-	CVector2(int s = 0, double* arr = nullptr);
-	void out() const;
-};
-class CVector3 :public CVector {
-public:
-	CVector3(const CVector3& other);
-	CVector3(int s = 0, double* arr = nullptr);
-	virtual void vout() const override; // override явно показывает переопределение функции родительского класса
-};
 
 #endif
