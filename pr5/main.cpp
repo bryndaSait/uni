eb331e484a0e7ea52d347f706b78d5b09f85b3cf
Mr_Bryanda_St <58851457+bryndaSait@users.noreply.github.com>
2026-06-05 12:44:35 +0300

    Update main.cpp

--- pr5/main.cpp
+++ pr5/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-
+    setlocale(LC_ALL, "rus");
 	while (true) {
 		int size1;
 
@@ -19,20 +19,45 @@ int main() {
 		for (int i = 0; i < size1; i++) {
 			cin >> arr1[i];
 		}
-		CVector v1(size1, arr1);
-		CVector v2(v1);
-		v2.out(); cout << endl;
-		CVector v3(std::move(v1));
-		v3.out(); cout << endl;
+        CVector v1(size1, arr1);
 
-		v1 = v2;
-		v1.out(); cout << endl;
-		CVector v4 = std::move(v1);
-		v4.out(); 
-		
-		delete[] arr1;
-		
 
+        // copy constructor
+        CVector v2(v1);
+        cout << "v2 (copy из v1): ";
+        v2.out();
+        cout << "v1 (начальный): ";
+        v1.out();
+        
+        // copy operator=
+        CVector v3;
+        v3 = v2;
+        cout << "v3 (сopy v2): ";
+        v3.out();
+        cout << "v2: ";
+        v2.out();
+       
+        
+
+        // move operator=
+        CVector v4;
+        v4 = v1 + v2;
+        cout << "v4 ( = v1): ";
+        v4.out();
+        cout << "v1 (начальный): ";
+        v1.out();
+
+        // move constructor
+        CVector v5;
+        v5.move(v3);
+        cout << "v5 (move из v3): ";
+        v5.out();
+        cout << "v3: ";
+        v3.out();
+        
+        delete[] arr1;
+		
+        //♥♥//
 	}
 
 	return 0;
